25c618aefd69d603b2b0c09c3980ac997edb67b6
Palmero96 <alvaropalmero@hotmail.com>
2020-01-07 16:10:32 +0100

    Se han implementado multithreading y multiprocessing para optimizar el rendimiento del programa, además se ha cambiado el svm_tester.py para tener una prueba sobre el svc mas claro

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ from _hog import hog
 from _imgProc import *
 from _FileVideoStream import FileVideoStream
 import threading
-import queue
+import multiprocessing as mp
+from multiprocessing import Queue
+#import queue
 from imutils.video import FPS
 import time
 
 BUF_SIZE = 1
-q = queue.Queue(BUF_SIZE)
+q = Queue(BUF_SIZE)
 
 def getPrediction(q):
+    # Open saved SVM model
+    loaded_model = pickle.load(open("bin/src/model.sav", "rb"))
+
     while True:
         if not q.empty():
             # Pasamos la foto de la mano por el SVM
@@ -29,110 +34,113 @@ def getPrediction(q):
             data = np.array([data])
             print(loaded_model.predict(data))
 
+def Imaq(q):
+    #esto es para cargar el clasificador de caras de OpenCV
+    face_cascade = cv.CascadeClassifier('bin/src/haarcascade_frontalface_default.xml')
+    # Inicializamos los intervalos de la piel
+    H_LowThreshold = 0
+    H_HighThreshold = 0
+    S_LowThreshold = 0
+    S_HighThreshold = 0
+    V_LowThreshold = 0
+    V_HighThreshold = 0
 
-############################################################
-#                   INICIO DEL MAIN                         #
-############################################################
+    #Enciendo la camara
+    fvs = FileVideoStream(0).start()
+    time.sleep(1.0)
+    #start FPS timer
+    fps = FPS().start()
 
-print("Inicio del programa")
+    # if not cap.isOpened():
+    #     print("Cannot open camera")
+    #     exit()
 
-th = threading.Thread(target=getPrediction, args=(q,))
-th.start()
-
-#esto es para cargar el clasificador de caras de OpenCV
-face_cascade = cv.CascadeClassifier('bin/src/haarcascade_frontalface_default.xml')
-# Inicializamos los intervalos de la piel
-H_LowThreshold = 0
-H_HighThreshold = 0
-S_LowThreshold = 0
-S_HighThreshold = 0
-V_LowThreshold = 0
-V_HighThreshold = 0
-
-# Open saved SVM model
-loaded_model = pickle.load(open("bin/src/model.sav", "rb"))
-
-#Enciendo la camara
-fvs = FileVideoStream(0).start()
-time.sleep(1.0)
-#start FPS timer
-fps = FPS().start()
-
-# if not cap.isOpened():
-#     print("Cannot open camera")
-#     exit()
-
-#Capturo el primer fame que sera el fondo inicial
-# ret, bg = cap.read()
-bg = fvs.read()
-fps.update()
-
-while True:
-    # # Capture frame-by-frame
-    # ret, frame = cap.read()
-    # # if frame is read correctly ret is True
-    # if not ret:
-    #     print("Can't receive frame (stream end?). Exiting ...")
-    #     break
-    frame = fvs.read()
-    base = frame
-
-    # Ponemos los rectangulos
-    cv.rectangle(base, (125, 255), (205, 295), (0, 255, 0), 2)
-    cv.rectangle(base, (125, 300), (205, 345), (0, 255, 0), 2)
-
-    # Ponemos algo de texto
-    font = cv.FONT_HERSHEY_SIMPLEX
-    # cv.putText(base, 'MutenRossi', (58, 58), font, 2, (255, 255, 255), 1, cv.LINE_AA)
-
-
-    if not q.full():
-        #Quitamos la cara
-        noFace = faceRemove(frame, face_cascade)
-        #Quitamos el fondo
-        BackGround = backgroundRemoval(noFace,bg)
-        #Binarizamos la imagen
-        imbin = binarization(BackGround, H_LowThreshold, H_HighThreshold, S_LowThreshold, S_HighThreshold, V_LowThreshold, V_HighThreshold)
-        #Situamos el Bounding box
-        bnd, origbnd = Bounding(imbin,frame)
-
-        cv.imshow('Bounding box', origbnd)
-
-        q.put(bnd)
-        # print("Existen {} fotos en la cola".format(q.qsize()))
-
-    else:
-        #Mostramos la imagen original
-        cv.imshow('Bounding box', base)
-
-    #teclas
-    k = cv.waitKey(1) & 0xFF
-    if k == ord('q'):#quit
-        break
-    elif k == ord('b'):#remove background
-        print('Obtenemos nuevo background')
-        bg = frame
-    elif k == ord('s'):#capture skin
-        print('Capturamos la piel del usuario')
-        thold = captureSkin(BackGround)
-
-        # Asign calculated thresholds to used ones
-        H_LowThreshold = thold[0]
-        H_HighThreshold = thold[1]
-        S_LowThreshold = thold[2]
-        S_HighThreshold = thold[3]
-        V_LowThreshold = thold[4]
-        V_HighThreshold = thold[5]
-
+    #Capturo el primer fame que sera el fondo inicial
+    # ret, bg = cap.read()
+    bg = fvs.read()
     fps.update()
 
+    while True:
+        # # Capture frame-by-frame
+        # ret, frame = cap.read()
+        # # if frame is read correctly ret is True
+        # if not ret:
+        #     print("Can't receive frame (stream end?). Exiting ...")
+        #     break
+        frame = fvs.read()
+        base = frame
 
-# When everything done, release the capture
-th.join()
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        # Ponemos los rectangulos
+        cv.rectangle(base, (125, 255), (205, 295), (0, 255, 0), 2)
+        cv.rectangle(base, (125, 300), (205, 345), (0, 255, 0), 2)
 
-# do a bit of cleanup
-cv.destroyAllWindows()
-fvs.stop()
+        # Ponemos algo de texto
+        font = cv.FONT_HERSHEY_SIMPLEX
+        # cv.putText(base, 'MutenRossi', (58, 58), font, 2, (255, 255, 255), 1, cv.LINE_AA)
+
+
+        if not q.full():
+            #Quitamos la cara
+            noFace = faceRemove(frame, face_cascade)
+            #Quitamos el fondo
+            BackGround = backgroundRemoval(noFace,bg)
+            #Binarizamos la imagen
+            imbin = binarization(BackGround, H_LowThreshold, H_HighThreshold, S_LowThreshold, S_HighThreshold, V_LowThreshold, V_HighThreshold)
+            #Situamos el Bounding box
+            bnd, origbnd = Bounding(imbin,frame)
+
+            cv.imshow('Bounding box', origbnd)
+
+            q.put(bnd)
+            # print("Existen {} fotos en la cola".format(q.qsize()))
+
+        else:
+            #Mostramos la imagen original
+            cv.imshow('Bounding box', base)
+
+        #teclas
+        k = cv.waitKey(1) & 0xFF
+        if k == ord('q'):#quit
+            break
+        elif k == ord('b'):#remove background
+            print('Obtenemos nuevo background')
+            bg = frame
+        elif k == ord('s'):#capture skin
+            print('Capturamos la piel del usuario')
+            thold = captureSkin(BackGround)
+
+            # Asign calculated thresholds to used ones
+            H_LowThreshold = thold[0]
+            H_HighThreshold = thold[1]
+            S_LowThreshold = thold[2]
+            S_HighThreshold = thold[3]
+            V_LowThreshold = thold[4]
+            V_HighThreshold = thold[5]
+
+        fps.update()
+
+    # Exiting
+    fps.stop()
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+    # do a bit of cleanup
+    cv.destroyAllWindows()
+    fvs.stop()
+
+
+############################################################
+#                   INICIO DEL MAIN                        #
+############################################################
+
+th = mp.Process(target=getPrediction, args=(q,), daemon=True)
+th2 = mp.Process(target=Imaq, args=(q,))
+
+if __name__ == '__main__':
+    print("Inicio del programa")
+
+    th.start()
+    th2.start()
+
+    # When everything done, release the capture
+    th2.join()
--- svm_tester.py
+++ svm_tester.py
@@ -11,23 +11,52 @@ from _hog import hog
 # Open saved SVM model
 loaded_model = pickle.load(open("bin/src/model.sav", "rb"))
 
-# Now that we have our model we'll import some images to test the model outside the dataset
-img = cv.imread('../../../../fotoc.png')
-img = img[8:199-8,8:199-8]
+opts = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'nothing')
+
 dim = (160, 160)
-img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
-img = np.float32(img)/255.0
-img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+data = np.zeros(shape=(len(opts),1620))
+num = 0
 
-img2 = cv.imread('../../../../fotoj.png')
-img2 = img2[8:199-8,8:199-8]
-img2 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
-img2 = np.float32(img2)/255.0
-img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+for o in opts:
+    path = '../../../../asl_alphabet_test/asl_alphabet_test/{}_test.jpg'.format(o)
 
-data = hog(img)
-data2 = hog(img2)
-data = np.array([data])
-data2 = np.array([data2])
+    img = cv.imread(path)
+    img = img[8:199-8,8:199-8]
+    img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+    img = np.float32(img)/255.0
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    data[num] = hog(img)
+    num=num+1
+
+
+# # Now that we have our model we'll import some images to test the model outside the dataset
+# img = cv.imread('../../../../fotoc.png')
+# img = img[8:199-8,8:199-8]
+# dim = (160, 160)
+# img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+# img = np.float32(img)/255.0
+# img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+#
+# # img2 = cv.imread('../../../../fotoj.png')
+# # img2 = img2[8:199-8,8:199-8]
+# # img2 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
+# # img2 = np.float32(img2)/255.0
+# # img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+#
+# img2 = cv.imread('../../../../asl_alphabet_test/asl_alphabet_test/nothing_test.jpg')
+# img2 = img2[8:199-8,8:199-8]
+# img2 = cv.resize(img2, dim, interpolation = cv.INTER_AREA)
+# img2 = np.float32(img2)/255.0
+# img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+
+# data = hog(img)
+# data2 = hog(img2)
+# data = np.array([data])
+# data2 = np.array([data2])
+print("Expected >> ")
+print(opts)
+print("Resulted >> ")
 print(loaded_model.predict(data))
-print(loaded_model.predict(data2))
+# print(loaded_model.predict(data2))
