8a71eb9115e527608d61c7281f3e00bbd0d8187c
Dolayo <44551274+Dolayo@users.noreply.github.com>
2020-01-07 16:11:19 +0100

    bound returns the hand

--- ImgProc.py
+++ ImgProc.py
@@ -159,30 +159,50 @@ def faceRemove(f):
 #que le guste a Alvaro
 #recibe la imagen binarizada y la imagen base
 #y devuelve la imagen en el formato que le guste a Alvaro y cuadrada
-def Bounding(binar,b):
-    contours, hierarchy = cv.findContours(binar,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-    cnt = max(contours, key = lambda x: cv.contourArea(x))
-    M = cv.moments(cnt)
+def Bounding(binar,f):
 
-    #Centroid
-    cx = int(M['m10'] / M['m00'])
-    cy = int(M['m01'] / M['m00'])
+    try:
+        contours, hierarchy = cv.findContours(binar,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        cnt = max(contours, key = lambda x: cv.contourArea(x))
 
-    #cv.rectangle(b, (cx-100, cy-100), (cx+130, cy+130), (255, 0, 0), 2)
-    x, y, w, h = cv.boundingRect(cnt)
-    mayor = 0
-    #me quedo con la dimension del rectangulo mayor
-    if(w>h):
-        mayor = w
-    else:
-        mayor = h
-        #pongo el rectangulo
-    cv.rectangle(b, (x, y), (x + mayor, y + mayor), (0, 255, 0), 2)
-    #me quedo con la mano solo
-    squared = b[x+3:x + mayor - 3,y+3:y - 3 + mayor]
-    #la cambio de tamanio
-    resized = cv.resize(squared,(320,320),interpolation=cv.INTER_AREA)
-    return b, resized
+        M = cv.moments(cnt)
+
+        #Centroid
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+
+        #cv.rectangle(b, (cx-100, cy-100), (cx+130, cy+130), (255, 0, 0), 2)
+        x, y, w, h = cv.boundingRect(cnt)
+        mayor = 0
+        #me quedo con la dimension del rectangulo mayor
+        if(w>h):
+            mayor = w
+        else:
+            mayor = h
+            #pongo el rectangulo
+        #Bounding rectangle
+        #X_ini = x-15
+        #Y_ini = y-15
+        #X_end = x + mayor + 15
+        #Y_end = y + mayor + 15
+        #cv.rectangle(b, (X_ini, Y_ini), (X_end, Y_end), (0, 255, 0), 2)
+
+        # Centroid rectangle
+        X_ini = cx - round(mayor/2)
+        Y_ini = cy - round(mayor/2) - 20
+        X_end = cx + round(mayor/2)
+        Y_end = cy + round(mayor/2)
+        cv.rectangle(f, (X_ini, Y_ini), (X_end, Y_end), (0, 0, 255), 2)
+
+        #me quedo con la mano solo
+        #squared = f[X_ini:X_end, Y_ini:Y_end]
+        squared = f[Y_ini:Y_end, X_ini:X_end]
+        # la cambio de tamanio
+        resized = cv.resize(squared, (320, 320), interpolation=cv.INTER_AREA)
+        return f, resized
+    except:
+        print('Nope')
+        return f, None
 
 #320x320
 print('hello world')
@@ -205,6 +225,11 @@ while True:
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+    ret, base = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
 
 
     # get dimensions of image
@@ -215,7 +240,6 @@ while True:
     #width = frame.shape[1]
     #channels = frame.shape[2]
 
-    base = frame
 
     # Ponemos los rectangulos
     cv.rectangle(base, (125, 255), (205, 295), (0, 255, 0), 2)
@@ -225,8 +249,6 @@ while True:
     font = cv.FONT_HERSHEY_SIMPLEX
     cv.putText(base, 'MutenRossi', (58, 58), font, 2, (255, 255, 255), 1, cv.LINE_AA)
 
-    # Mostramos la imagen
-    #cv.imshow('frame', base)
 
     #Quitamos la cara
     noFace = faceRemove(frame)
@@ -238,14 +260,19 @@ while True:
     bin = binarization(BackGround, H_LowThreshold, H_HighThreshold, S_LowThreshold, S_HighThreshold, V_LowThreshold, V_HighThreshold)
 
     #Situamos el Bounding box
-    bnd,bnd_res = Bounding(bin,base)
+    bnd,bnd_res = Bounding(bin,frame)
 
-    print('huauauauauauuaa',bnd_res.shape[0])
-    print('huauauauauauuaa',bnd_res.shape[1])
+    #print('huauauauauauuaa',bnd_res.shape[0])
+    #print('huauauauauauuaa',bnd_res.shape[1])
 
-    cv.imshow('Foreground',BackGround)
-    cv.imshow('user skin', bin)
-    cv.imshow('Bounding box', bnd)
+    try:
+        # Mostramos la imagen
+        cv.imshow('frame', base)
+        cv.imshow('Foreground',BackGround)
+        cv.imshow('user skin', bin)
+        cv.imshow('Bounding box', bnd_res)
+    except:
+        print('empty image to show')
 
 
     #teclas
@@ -258,6 +285,9 @@ while True:
     elif k == ord('s'):#capture skin
         print('Capturamos la piel del usuario')
         captureSkin(BackGround)
+    elif k == ord('f'):#capture image
+        print('Guardamos la imagen resized')
+        cv.imwrite('mano.png', bnd_res)
 
 # When everything done, release the capture
 cap.release()
